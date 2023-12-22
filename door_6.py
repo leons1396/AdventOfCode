c7f19e0edcdbdc76c9c7e328a57225b5727b8275
Leon <scle1066@h-ka.de>
2023-12-22 10:16:33 +0100

    DOOR 6 Complete.

---
+++ door_6.py
@@ -0,0 +1,25 @@
+time = [46689866]
+distance = [358105418071080]
+
+l = []
+for i in range(0, len(time)):
+    t = time[i]
+    best = distance[i]
+    ways_to_beat = 0
+    for j, sec in enumerate(range(1, t)):
+        time_left = t - sec
+        speed = sec
+        x = speed * time_left
+
+        if x <= best:
+            continue
+        ways_to_beat += 1
+
+    l.append(ways_to_beat)
+
+print(l)
+
+result = 1
+for x in l:
+    result = result * x
+print(result)
